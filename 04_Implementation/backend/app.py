222c9b47f24709bafdbf96957089fccf247f3fb1
EliasKng <elias-koenig@web.de>
2021-12-15 15:57:17 +0100

    Define data receiving endpoint

--- 04_Implementation/backend/app.py
+++ 04_Implementation/backend/app.py
@@ -1,8 +1,54 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+from DataStorage import *
 
+
+class IndexRowNames(str, Enum):
+    Segment = 'Segment'
+    Country = 'Country'
+    Product = 'Product'
+    DiscountBand = "Discount Band"
+
+
+class ValueRowNames(str, Enum):
+    UnitsSold = "Units Sold"
+    ManufacturingPrice = "Manufacturing Price"
+    SalePrice = "Sale Price"
+    GrossSales = "Gross Sales"
+    Discounts = "Discounts"
+    Sales = "Sales"
+    Cogs = "COGS"
+    Profit = "Profit"
+    Month = "Month"
+    Year = "Year"
+
+
+class Aggregates(str, Enum):
+    sum = "sum"
+    mean = "mean"
+    min = "min"
+    max = "max"
+    count = "count"
+
+
+class DataRequestBody(BaseModel):
+    values_row_name: ValueRowNames
+    index_row_name: IndexRowNames
+    aggregate: Aggregates
+
+
+data_storage = DataStorage()
+
+
+# ----- API and endpoints -----
 app = FastAPI()
 
 
-@app.get("/hello/{name}")
-async def hello(name):
-    return "welcome3 " + name
+@app.post("/data")
+async def data(request_body: DataRequestBody):
+    return data_storage.prepare_dataframe(
+        request_body.index_row_name,
+        request_body.values_row_name,
+        request_body.aggregate
+    )
