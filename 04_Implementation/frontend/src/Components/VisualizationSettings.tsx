5a44c578b9f7c30c1b9ec97e9430057952a56f50
EliasKng <elias-koenig@web.de>
2021-12-17 11:30:22 +0100

    Request Schema from API

--- 04_Implementation/frontend/src/Components/VisualizationSettings.tsx
+++ 04_Implementation/frontend/src/Components/VisualizationSettings.tsx
@@ -3,29 +3,32 @@ import {VisualizationSetting} from './VisualizationSetting';
 
 interface VisualizationSettingsProps {
 	// eslint-disable-next-line
-	schema: Record<string, any>
+	apiSchema: Record<string, any>
 }
 
 export class VisualizationSettings extends Component<VisualizationSettingsProps> {
 	render() {
-		console.log('InVisSettings:');
-		console.log(this.props.schema);
+		if (this.props.apiSchema) {
+			return <div className='visualization-settings'>
+				<h2>Visualization Settings</h2>
+				<VisualizationSetting title='x-Axis' entries={['Segment', 'Country', 'Product', 'Discount Band']}/>
+				<VisualizationSetting title='y-Axis' entries={[
+					'Units Sold',
+					'Manufacturing Price',
+					'Sale Price',
+					'Gross Sales',
+					'Discounts',
+					'Sales',
+					'COGS',
+					'Profit',
+					'Month',
+					'Year',
+				]}/>
+				<VisualizationSetting title='Aggregate' entries={['sum', 'mean', 'count', 'min', 'max']}/>
+			</div>;
+		}
 		return <div className='visualization-settings'>
-			<h2>{this.props.schema??'Visualization Settings'}</h2>
-			<VisualizationSetting title='x-Axis' entries={['Segment', 'Country', 'Product', 'Discount Band']}/>
-			<VisualizationSetting title='y-Axis' entries={[
-				'Units Sold',
-				'Manufacturing Price',
-				'Sale Price',
-				'Gross Sales',
-				'Discounts',
-				'Sales',
-				'COGS',
-				'Profit',
-				'Month',
-				'Year',
-			]}/>
-			<VisualizationSetting title='Aggregate' entries={['sum', 'mean', 'count', 'min', 'max']}/>
+			<h2>Loading ...</h2>
 		</div>;
 	}
 }
