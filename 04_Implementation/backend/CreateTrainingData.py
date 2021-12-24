8012d429adc3fa17fd4fad8062694ab69cba7bf3
EliasKng <elias-koenig@web.de>
2021-12-24 09:38:32 +0100

    Create Training Table and Title

--- 04_Implementation/backend/CreateTrainingData.py
+++ 04_Implementation/backend/CreateTrainingData.py
@@ -5,17 +5,27 @@ class CreateTrainingData:
     def __init__(self, data_storage: DataStorage):
         self.data_storage = data_storage
 
-    def createTrainingData(
+    def createTrainingTable(
             self,
             index_row_name: DataStorage.IndexRowName,
             values_row_name: DataStorage.ValuesRowName,
             aggregate: DataStorage.Aggregate
     ):
-        df = self.data_storage.prepare_dataframe(index_row_name, values_row_name, aggregate)
-        print(df)
+        return self.data_storage.prepare_dataframe(index_row_name, values_row_name, aggregate)
 
     def createTrainingDataForDataset(self):
+        i = 0
         for index_row_name in DataStorage.IndexRowName:
             for values_row_name in DataStorage.ValuesRowName:
                 for aggregate in DataStorage.Aggregate:
-                    self.createTrainingData(index_row_name, values_row_name, aggregate)
+                    df = self.createTrainingTable(index_row_name, values_row_name, aggregate)
+                    title = '{aggregate} of {values_row_name} by {index_row_name}'.format(
+                        aggregate=aggregate, values_row_name=values_row_name, index_row_name=index_row_name)
+                    print(df)
+                    print(title)
+        print(i)
+
+
+
+data_storage = DataStorage()
+CreateTrainingData(data_storage).createTrainingDataForDataset()
