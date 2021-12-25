17915847b071ae03876f779db5992515e4ffbd64
EliasKng <elias-koenig@web.de>
2021-12-25 14:49:42 +0100

    Create Training Table and Title ans save them to separate files

--- 04_Implementation/backend/CreateTrainingData.py
+++ 04_Implementation/backend/CreateTrainingData.py
@@ -1,11 +1,12 @@
 from DataStorage import *
+from os import path
 
 
 class CreateTrainingData:
     def __init__(self, data_storage: DataStorage):
         self.data_storage = data_storage
 
-    def createTrainingTable(
+    def create_training_table(
             self,
             index_row_name: DataStorage.IndexRowName,
             values_row_name: DataStorage.ValuesRowName,
@@ -13,19 +14,31 @@ class CreateTrainingData:
     ):
         return self.data_storage.prepare_dataframe(index_row_name, values_row_name, aggregate)
 
-    def createTrainingDataForDataset(self):
-        i = 0
+    def create_training_data(self):
+        data = []
         for index_row_name in DataStorage.IndexRowName:
             for values_row_name in DataStorage.ValuesRowName:
                 for aggregate in DataStorage.Aggregate:
-                    df = self.createTrainingTable(index_row_name, values_row_name, aggregate)
+                    df = self.create_training_table(index_row_name.value, values_row_name.value, aggregate.value)
                     title = '{aggregate} of {values_row_name} by {index_row_name}'.format(
                         aggregate=aggregate, values_row_name=values_row_name, index_row_name=index_row_name)
-                    print(df)
-                    print(title)
-        print(i)
+                    data.append((title, df))
+        return data
+
+
+def save_training_data(training_data):
+    assert path.exists('./trainingData')
+    i = 6763
+    for (title, df) in training_data:
+        with open('trainingData/Titles/' + str(i) + '.txt', 'w') as text_file:
+            text_file.write(title)
+            text_file.close()
+        df.to_csv('trainingData/Tables/' + str(i) + '.csv')
+        i += 1
 
 
 
 data_storage = DataStorage()
-CreateTrainingData(data_storage).createTrainingDataForDataset()
+ctd = CreateTrainingData(data_storage)
+data = ctd.create_training_data()
+save_training_data(data)
