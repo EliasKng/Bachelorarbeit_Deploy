08ea67b77888079f4667fea94976a18965900623
EliasKng <elias-koenig@web.de>
2021-12-27 15:48:14 +0100

    Create a class that returns a summary matching a certain title

---
+++ 04_Implementation/backend/FindGeneratedSummary.py
@@ -0,0 +1,25 @@
+from DataStorage import *
+import json
+from os import path
+
+
+class FindGeneratedSummary:
+    summaries_by_title = []
+    titles = []
+    path_to_summaries = './Summaries/'
+
+    def __init__(self):
+        summary_paths = os.listdir(self.path_to_summaries)
+
+        for summary_path in summary_paths:
+            with open(self.path_to_summaries + summary_path, 'r', encoding='utf-8') as titleFile:
+                summary = json.load(titleFile)
+                title = summary["title"]
+                summary = ''.join(summary["summary"])
+                self.summaries_by_title.append((title, summary))
+                self.titles.append(title)
+
+    def get_summary(self, title):
+        index = self.titles.index(title)
+        print(self.summaries_by_title[index][1])
+
