38b24abe62018b96d31ecb87795ffe203b1209c3
EliasKng <elias-koenig@web.de>
2021-12-27 16:20:25 +0100

    Send the title and generated summary with each data request

--- 04_Implementation/backend/app.py
+++ 04_Implementation/backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import json
 from pydantic import BaseModel
 from DataStorage import *
+from SummaryStorage import *
 from fastapi.middleware.cors import CORSMiddleware
 
 class DataRequestBody(BaseModel):
@@ -11,6 +12,7 @@ class DataRequestBody(BaseModel):
 
 
 data_storage = DataStorage()
+summary_storage = SummaryStorage()
 
 
 # ----- API and endpoints -----
@@ -40,18 +42,27 @@ async def data(request_body: DataRequestBody):
         request_body.values_row_name.value,
         request_body.aggregate.value
     )
+
+    #Process title, which is sent, but also used as identifier to find the correct summary
+    title = get_title(
+        request_body.aggregate.value,
+        request_body.values_row_name.value,
+        request_body.index_row_name.value
+    )
+    summary = summary_storage.get_summary(title)
+
+    dict = {
+        "title": title,
+        "summary": summary
+    }
+
     json_string = prepared_df.to_json(orient='table')
-    jsonObj = json.loads(json_string)
-    return jsonObj
+    json_obj = json.loads(json_string)
+    json_obj.append(dict)
+    return json_obj
 
 
-@app.get("/dataset-fields")
-async def dataset_fields():
-    a = IndexRowName()
-    print(IndexRowName.toJSON(a))
-    return 'success'
-
-
-@app.get("/test")
-async def data():
-    return 'helloWorld'
+# --- help functions ---
+def get_title(aggregate, values_row_name, index_row_name):
+    return '{aggregate} of {values_row_name} by {index_row_name}'.format(
+        aggregate=aggregate, values_row_name=values_row_name, index_row_name=index_row_name)
