42a285822767f3c931b10aa72864c434c03fe0b2
EliasKng <elias-koenig@web.de>
2022-01-02 13:12:04 +0100

    Code Cleanup

--- 04_Implementation/frontend/src/App.tsx
+++ 04_Implementation/frontend/src/App.tsx
@@ -3,8 +3,7 @@ import './Scss/App.scss';
 import {VisualizationSettings} from './Components/VisualizationSettings';
 import {VisChartjs} from './Components/VisChartjs';
 import {Summary} from './Components/Summary';
-
-const REQUEST_URL = 'http://localhost:8000';
+import {getRequest, postRequest, wait} from './Components/Requests';
 
 class App extends Component {
 	private apiSchemaEndpoint = '/openapi.json';
@@ -15,6 +14,7 @@ class App extends Component {
 		this.state = {
 			apiSchema: undefined,
 			summary: '',
+			visSchema: '',
 			visColorCode: ['rgba(106, 110, 229)'],
 			highlightedElements: {
 				bars: [0,3],
@@ -97,63 +97,3 @@ class App extends Component {
 
 export default App;
 
-// eslint-disable-next-line
-async function getRequest(endpoint: string): Promise<Record<string, any>> {
-	try {
-		const response = await fetch(
-			REQUEST_URL + endpoint
-		);
-		return await response.json();
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-// eslint-disable-next-line
-async function postRequest(endpoint: string, requestBody: Record<string, any>): Promise<Record<string, any>> {
-	try {
-		const response = await fetch(
-			REQUEST_URL + endpoint, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(requestBody),
-			}
-		);
-		return await response.json();
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-// eslint-disable-next-line
-async function testFetchPost() {
-	try {
-		console.log('testFetchPost');
-		const requestBody = {
-			// 'values_row_name': 'Units Sold',
-			// 'index_row_name': 'Segment',
-			// 'aggregate': 'sum',
-		};
-		const response = await fetch(
-			// 'https://29756-3000.codesphere.com/test',
-			'http://localhost:8000/data', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(requestBody),
-			}
-		);
-		const data = await response.json();
-		console.log(data);
-	} catch (error) {
-		console.log(error);
-	}
-
-}
-
-function wait(delay) {
-	return new Promise((resolve) => setTimeout(resolve, delay));
-}
