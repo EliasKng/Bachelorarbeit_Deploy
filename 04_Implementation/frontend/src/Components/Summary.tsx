39e3b137176d18d8435836259e9691a651777c67
EliasKng <elias-koenig@web.de>
2022-01-02 14:34:26 +0100

    Send Visdata when analyzing Summary

--- 04_Implementation/frontend/src/Components/Summary.tsx
+++ 04_Implementation/frontend/src/Components/Summary.tsx
@@ -5,7 +5,7 @@ import {getSummaryAnalysis} from './SummaryAnalyzer';
 interface SummaryProps {
 	summary: string,
 	updateSummary: (event: Event) => void
-	visSchema: Record<string, any>
+	visData: Record<string, any>
 }
 
 export class Summary extends Component<SummaryProps> {
@@ -32,6 +32,6 @@ export class Summary extends Component<SummaryProps> {
 	};
 
 	analyzeSummary() {
-		getSummaryAnalysis(this.props.summary, this.props.visSchema);
+		getSummaryAnalysis(this.props.summary, this.props.visData);
 	}
 }
