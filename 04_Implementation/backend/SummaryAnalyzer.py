30fb94e1d50316bfc90a7ad18d46a07c8558326a
EliasKng <elias-koenig@web.de>
2022-01-06 09:19:28 +0100

    Improve recognizing keys and lebels by tokenizing them

--- 04_Implementation/backend/SummaryAnalyzer.py
+++ 04_Implementation/backend/SummaryAnalyzer.py
@@ -6,8 +6,15 @@ from typing import List
 
 def analyze_summary(summary: string, vis_data: List[dict]):
     sentences = tokenize_sentences(summary)
+
+    # Extract kkeys and labels as list
     keys = list(vis_data[0].keys())
     labels = [datum[keys[0]] for datum in vis_data]
+
+    # Split each key and label by whitespace ('Discount Band' => ['Discount', 'Band'])
+    keys = [key.split() for key in keys]
+    labels = [label.split() for label in labels]
+
 
     sentence_mappings = []
 
@@ -19,17 +26,29 @@ def analyze_summary(summary: string, vis_data: List[dict]):
 
 def analyze_sentence(sentence, labels, keys):
     mapped_labels = []
-    for label in labels:
-        if label in sentence:
-            mapped_labels.append(label)
+    for label_tokens in labels:
+        are_all_tokens_in_sentence = True
+        for label_token in label_tokens:
+            if label_token not in sentence:
+                are_all_tokens_in_sentence = False
+                continue
+
+        if are_all_tokens_in_sentence:
+            mapped_labels.append(' '.join(label_tokens))
 
     # Only search for mapped keys if there were no labels found in the sentence
     # This prevents for every: "The Product with the highest Sales was Amarillo" to highlight Sales and Product
     mapped_keys = []
     if len(mapped_labels) == 0:
-        for key in keys:
-            if key in sentence:
-                mapped_keys.append(key)
+        for key_tokens in keys:
+            are_all_tokens_in_sentence = True
+            for key_token in key_tokens:
+                if key_token not in sentence:
+                    are_all_tokens_in_sentence = False
+                    continue
+
+            if are_all_tokens_in_sentence:
+                mapped_keys.append(' '.join(key_tokens))
 
     return {
         "sentence": sentence,
