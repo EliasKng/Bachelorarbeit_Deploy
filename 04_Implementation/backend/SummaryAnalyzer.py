2632072b6af8be5aca51a1a5889bc5bc1341b81d
EliasKng <elias-koenig@web.de>
2022-01-06 09:25:32 +0100

    Improve recognizing keys and lebels by tokenizing them

--- 04_Implementation/backend/SummaryAnalyzer.py
+++ 04_Implementation/backend/SummaryAnalyzer.py
@@ -11,11 +11,6 @@ def analyze_summary(summary: string, vis_data: List[dict]):
     keys = list(vis_data[0].keys())
     labels = [datum[keys[0]] for datum in vis_data]
 
-    # Split each key and label by whitespace ('Discount Band' => ['Discount', 'Band'])
-    keys = [key.split() for key in keys]
-    labels = [label.split() for label in labels]
-
-
     sentence_mappings = []
 
     for sentence in sentences:
@@ -25,8 +20,12 @@ def analyze_summary(summary: string, vis_data: List[dict]):
 
 
 def analyze_sentence(sentence, labels, keys):
+    # Split each key and label by whitespace ('Discount Band' => ['Discount', 'Band'])
+    keys_tokenized = [key.split() for key in keys]
+    labels_tokenized = [label.split() for label in labels]
+
     mapped_labels = []
-    for label_tokens in labels:
+    for label, label_tokens in zip(labels, labels_tokenized):
         are_all_tokens_in_sentence = True
         for label_token in label_tokens:
             if label_token not in sentence:
@@ -34,13 +33,13 @@ def analyze_sentence(sentence, labels, keys):
                 continue
 
         if are_all_tokens_in_sentence:
-            mapped_labels.append(' '.join(label_tokens))
+            mapped_labels.append(label)
 
     # Only search for mapped keys if there were no labels found in the sentence
     # This prevents for every: "The Product with the highest Sales was Amarillo" to highlight Sales and Product
     mapped_keys = []
     if len(mapped_labels) == 0:
-        for key_tokens in keys:
+        for key, key_tokens in zip(keys, keys_tokenized):
             are_all_tokens_in_sentence = True
             for key_token in key_tokens:
                 if key_token not in sentence:
@@ -48,7 +47,7 @@ def analyze_sentence(sentence, labels, keys):
                     continue
 
             if are_all_tokens_in_sentence:
-                mapped_keys.append(' '.join(key_tokens))
+                mapped_keys.append(key)
 
     return {
         "sentence": sentence,
