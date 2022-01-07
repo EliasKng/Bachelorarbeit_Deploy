573f7546a21cc60634f7b2509a4419cb9e74e7e1
EliasKng <elias-koenig@web.de>
2022-01-07 19:10:45 +0100

    Generate List Value sentence

--- 04_Implementation/backend/SummaryStatementGenerator.py
+++ 04_Implementation/backend/SummaryStatementGenerator.py
@@ -7,6 +7,17 @@ def generate_summary_statements(selected: List[int], vis_data: List[dict]):
     keys = list(vis_data[0].keys())
     labels = [datum[keys[0]] for datum in vis_data]
 
-    print(keys)
-    print(labels)
-    print(selected)
+    # Sort selected indices ascending
+    selected.sort()
+
+    filtered_vis_data = [vis_data[index] for index in selected]
+
+    print(filtered_vis_data)
+
+    print(generate_list_value(keys, filtered_vis_data[0][keys[0]], filtered_vis_data[0][keys[1]]))
+
+
+def generate_list_value(keys, label, value):
+    print(keys, label, value)
+
+    return label + ' had ' + keys[1] + ' of ' + str(value)
